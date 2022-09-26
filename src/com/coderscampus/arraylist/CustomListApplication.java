98a3cfb03b41c379c12b408dff644f8684343587
TheJohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-09-26 16:07:42 -0400

    Add some test cases

--- src/com/coderscampus/arraylist/CustomListApplication.java
+++ src/com/coderscampus/arraylist/CustomListApplication.java
@@ -6,8 +6,20 @@ public class CustomListApplication {
 	public static void main(String[] args) {
 		CustomList<String> yourList = new CustomArrayList<>();
 		yourList.add("John");
-
+		yourList.add("Sam");
+		yourList.add("Maggie");
+		yourList.add("John");
+		yourList.add("Sam");
+		yourList.add("Maggie");
+		yourList.add("John");
+		yourList.add("Sam");
+		yourList.add("Maggie");
+		yourList.add("John");
+		yourList.add("Sam");
+		yourList.add("Maggie");
 		System.out.println(yourList);
+		System.out.println(yourList.get(11));
+		System.out.println(yourList.getSize());
 	
 	}
 
