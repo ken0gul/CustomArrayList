638642bbd3aa31bcab45e0475616499f7767bad1
TheJohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-10-02 13:28:04 -0400

    Double the backing array's size when it is full
    Refactored the code to update the backing array's size every time it reaches the max capacity.

--- src/com/coderscampus/arraylist/CustomArrayList.java
+++ src/com/coderscampus/arraylist/CustomArrayList.java
@@ -4,57 +4,49 @@ import java.util.Arrays;
 
 public class CustomArrayList<T> implements CustomList<T> {
 	Object[] items = new Object[10];
-
-	// Create a counter variable to keep track of items added to our array
+	Object[] myArrayList = new Object[items.length];
 	int counter = 0;
-
 	@Override
 	public boolean add(T item) {
-		// Increment by 1 each time this method gets called
-		counter = counter + 1;
-
-		// Create a variable to store the value we get from counter
-		int newSize = counter;
-
-		// loop items array through counter of times
-		for (int i = 0; i < counter; i++) {
-
-			// Add "item" if the current item of the array is null.
+		// TODO Auto-generated method stub
+		counter++;
+		if(counter == items.length) {
+			int newSize = (items.length) *2;
+			items = Arrays.copyOf(items, newSize);
+		}
+		for (int i = 0; i < items.length; i++) {
 			if (items[i] == null) {
 				items[i] = item;
-				items = Arrays.copyOf(items, newSize);
-
-//				break;
+				break;
 			}
-
-			else
-				// If the current item "items[i]" is not null then call Arrays.copyOf with
-				// newSize
-
-				items = Arrays.copyOf(items, newSize);
-
+			
 		}
-
+		
+		for(Object x : items) {
+			if(x != null) {
+			myArrayList = Arrays.copyOf(items, counter);
+			
+			}
+		}
 		return true;
-
 	}
 
 	@Override
 	public int getSize() {
-		return items.length;
+		return myArrayList.length;
 	}
 
 	@Override
 	public T get(int index) {
-		for (int i = 0; i < items.length;) {
-			return (T) items[index];
+		for (int i = 0; i < myArrayList.length;) {
+			return (T) myArrayList[index];
 		}
 		return null;
 	}
 
 	@Override
 	public String toString() {
-		return "CustomArrayList [items=" + Arrays.toString(items) + ", getSize()=" + getSize() + ", getClass()="
+		return "CustomArrayList [items=" + Arrays.toString(myArrayList) + ", getSize()=" + getSize() + ", getClass()="
 				+ getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
 	}
 
